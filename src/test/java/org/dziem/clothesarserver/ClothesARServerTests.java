2088b8906f9ce76e0aa1f465767ffc04cc66c34f
Jakub Dziem <jakub.dziem9@gmail.com>
2025-08-25 20:44:54 +0200

    add: test profile for main test class

--- src/test/java/org/dziem/clothesarserver/ClothesARServerTests.java
+++ src/test/java/org/dziem/clothesarserver/ClothesARServerTests.java
@@ -2,8 +2,19 @@ package org.dziem.clothesarserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
-@SpringBootTest(properties = "spring.liquibase.enabled=false")
+@SpringBootTest(properties = {
+        "spring.liquibase.enabled=false",
+        "spring.datasource.url=jdbc:h2:mem:testdb",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.data.redis.host=localhost",
+        "spring.data.redis.port=6379"
+})
+@MockBean(StringRedisTemplate.class)
 class ClothesARServerTests {
 
     @Test
