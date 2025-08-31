c04d060698ad1aff7eb4b6a51831c91e76499a52
Jakub Dziem <jakub.dziem9@gmail.com>
2025-08-31 20:55:52 +0200

    feat: use LocalDate instead of Date, missed one :D

--- src/main/java/org/dziem/clothesarserver/dto/AddPieceOfClothingDTO.java
+++ src/main/java/org/dziem/clothesarserver/dto/AddPieceOfClothingDTO.java
@@ -1,17 +1,21 @@
 package org.dziem.clothesarserver.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class AddPieceOfClothingDTO {
     private String name;
     private String imageUrl;
     private String category;
-    private Date purchaseDate;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate purchaseDate;
     private String size;
     private String condition;
     private String note;
@@ -19,4 +23,18 @@ public class AddPieceOfClothingDTO {
     private List<String> tags;
     private String brand;
     private String arUrl;
+
+    public AddPieceOfClothingDTO(UpdatePieceOfClothingDTO updatePieceOfClothingDTO) {
+        this.name = updatePieceOfClothingDTO.getName();
+        this.imageUrl = updatePieceOfClothingDTO.getImageUrl();
+        this.category = updatePieceOfClothingDTO.getCategory();
+        this.purchaseDate = updatePieceOfClothingDTO.getPurchaseDate();
+        this.size = updatePieceOfClothingDTO.getSize();
+        this.condition = updatePieceOfClothingDTO.getCondition();
+        this.note = updatePieceOfClothingDTO.getNote();
+        this.price = updatePieceOfClothingDTO.getPrice();
+        this.tags = updatePieceOfClothingDTO.getTags();
+        this.brand = updatePieceOfClothingDTO.getBrand();
+        this.arUrl = updatePieceOfClothingDTO.getArUrl();
+    }
 }
