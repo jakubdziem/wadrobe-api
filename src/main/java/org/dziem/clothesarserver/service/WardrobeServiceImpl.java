a770e335ec93cdd1823a975b74e8032dde4892e7
Jakub Dziem <jakub.dziem9@gmail.com>
2025-08-31 21:00:25 +0200

    feat: sorting of wardrobe preview entities and add to it occasions and tags

--- src/main/java/org/dziem/clothesarserver/service/WardrobeServiceImpl.java
+++ src/main/java/org/dziem/clothesarserver/service/WardrobeServiceImpl.java
@@ -1,21 +1,28 @@
 package org.dziem.clothesarserver.service;
 
 import org.dziem.clothesarserver.dto.PieceOfClothingPreviewDTO;
+import org.dziem.clothesarserver.repository.OccasionRepository;
 import org.dziem.clothesarserver.repository.PieceOfClothingRepository;
+import org.dziem.clothesarserver.repository.TagRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class WardrobeServiceImpl implements WardrobeService {
     private final PieceOfClothingRepository pieceOfClothingRepository;
+    private final OccasionRepository occasionRepository;
     private final UserService userService;
+    private final TagRepository tagRepository;
 
-    public WardrobeServiceImpl(PieceOfClothingRepository pieceOfClothingRepository, UserService userService) {
+    public WardrobeServiceImpl(PieceOfClothingRepository pieceOfClothingRepository, OccasionRepository occasionRepository, UserService userService, TagRepository tagRepository) {
         this.pieceOfClothingRepository = pieceOfClothingRepository;
+        this.occasionRepository = occasionRepository;
         this.userService = userService;
+        this.tagRepository = tagRepository;
     }
 
     @Override
@@ -25,12 +32,16 @@ public class WardrobeServiceImpl implements WardrobeService {
         List<PieceOfClothingPreviewDTO> pieceOfClothingPreviewDTOs =
                 pieceOfClothingRepository.findPieceOfClothingPreviewListByUserId(
                                 userUUID)
-                        .stream().map(projection ->
-                            new PieceOfClothingPreviewDTO(
+                        .stream().map(projection -> {
+                            PieceOfClothingPreviewDTO pieceOfClothingPreviewDTO = new PieceOfClothingPreviewDTO(
                                     projection.getIsFavorite(),
                                     projection.getImageUrl(),
                                     projection.getWearCount(),
-                                    projection.getPieceOfClothingId()))
+                                    projection.getPieceOfClothingId());
+                            pieceOfClothingPreviewDTO.setOccasions(occasionRepository.findAllDTOsByPieceOfClothing(projection.getPieceOfClothingId()));
+                            pieceOfClothingPreviewDTO.setTags(tagRepository.findNamesByPieceOfClothingIds(projection.getPieceOfClothingId()));
+                            return pieceOfClothingPreviewDTO;
+                        }).sorted(Comparator.comparingLong(PieceOfClothingPreviewDTO::getPieceOfClothingId))
                         .toList();
 
         return ResponseEntity.ok(pieceOfClothingPreviewDTOs);
